7eb3b08c11e379a6568bca41953130ffd94973c1
Keeron1 <keeron9@gmail.com>
2026-04-22 15:54:13 +0200

    Added install commands for MMPose. Fixed the main notebook's format.

--- setup.ps1
+++ setup.ps1
@@ -1,11 +1,24 @@
 Write-Host "Creating virtual environment..."
-python -m venv venv
+py -3.10 -m venv venv
 
 Write-Host "Upgrading pip..."
-.\venv\Scripts\python -m pip install --upgrade pip
+.\venv\Scripts\python -m pip install --upgrade pip setuptools wheel
 
-Write-Host "Installing dependencies..."
-.\venv\Scripts\pip install -r requirements.txt
+Write-Host "Installing PyTorch (CPU only)..."
+.\venv\Scripts\python -m pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 `
+  --index-url https://download.pytorch.org/whl/cpu
+
+Write-Host "Locking NumPy..."
+.\venv\Scripts\python -m pip install "numpy>=1.26,<2"
+
+Write-Host "Installing base dependencies..."
+.\venv\Scripts\pip install -r requirements.txt --no-deps
+
+Write-Host "Installing OpenMMLab Tools"
+.\venv\Scripts\python -m pip install openmim
+.\venv\Scripts\python -m mim install mmengine
+.\venv\Scripts\python -m mim install "mmcv==2.2.0"
+.\venv\Scripts\python -m mim install mmpose
 
 Write-Host "Installing Jupyter kernel..."
 .\venv\Scripts\python -m pip install ipykernel
---
+++ src/pose_estimator/hrnet_pose.py
@@ -0,0 +1,8 @@
+from mmpose.apis import init_model, inference_topdown
+import numpy as np
+
+class HrNetPose:
+    def __init__(self):
+        print("ddd")
+
+    
