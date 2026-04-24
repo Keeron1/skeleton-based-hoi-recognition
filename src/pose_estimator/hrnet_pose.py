2a4d681e3b30c7c08f4f6ce25cfab87eeae10e33
Keeron1 <keeron9@gmail.com>
2026-04-24 14:15:08 +0200

    Added MMPose init and inference functions. Need to parse DeepSORT output to extract bboxes and select HrNet model and ckpt.

--- src/pose_estimator/hrnet_pose.py
+++ src/pose_estimator/hrnet_pose.py
@@ -1,8 +1,15 @@
 from mmpose.apis import init_model, inference_topdown
-import numpy as np
 
 class HrNetPose:
-    def __init__(self):
-        print("ddd")
+    def __init__(self, model_cfg, ckpt, device):
+        # config path, model weights, device where the anchors will be put
+        self.model = init_model(model_cfg, ckpt, device)
 
-    
+    def infer(self, frame, bboxes):
+        if len(bboxes) == 0:
+            print("Empty bboxes detected while trying to infer poses...")
+            return []
+        
+        # model, frame, bboxes, bboxes format
+        batch_results = inference_topdown(self.model, frame, bboxes, "xyxy")
+        return batch_results
